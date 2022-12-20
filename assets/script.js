89254bfd1d297cd2e52b339368cf0d77de01bd33
Brad Ingram <brad.ingram03@gmail.com>
2022-12-19 22:09:24 -0600

    added variables to script.js and fixed broken stylesheet link in index.html

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,28 @@
  // Assignment Code
  var generateBtn = document.querySelector("#generate");
 
+ var pwLength; //confirm password length
+ var confSpec; //confirm special characters
+ var confUp; //confirm uppercase
+ var confLo; // confirm lowercase
+ var confNum; // confirm numbers
+ var userSel; // user selection
  
-
+ var specialCharacters = [ ! " # $ % & '( ) * + , - . / : ; < = > ? @ [ \ ]^ _ ` { | } ~ " ];
+ var upper = ["A B C D E F G H I J K L M N O P Q R S T U V W X Y Z"]
+ var lower = ["a b c d e f g h i j k l m n o p q r s t u v w x y z"]
+ var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+ 
+ 
  // Write password to the #password input
-function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-  
-    passwordText.value = password;
-  
-  }
-  
-
-  // Add event listener to generate button
-  generateBtn.addEventListener("click", writePassword);
+ function writePassword() {
+   var password = generatePassword();
+   var passwordText = document.querySelector("#password");
+ 
+   passwordText.value = password;
+ 
+ }
+ 
+ // Add event listener to generate button
+ generateBtn.addEventListener("click", writePassword);
+ 
