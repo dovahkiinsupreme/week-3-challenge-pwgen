a9fba05c499ae3e6c7da6d96c9b90004ee4a51a1
Brad Ingram <brad.ingram03@gmail.com>
2022-12-21 22:51:26 -0600

    went a different direction for D.R.Y. code. getting close.

--- assets/script.js
+++ assets/script.js
@@ -2,142 +2,70 @@
  var generateBtn = document.querySelector("#generate");
 
  var pwLength; //confirm password length
- var confSpec; //confirm special characters
- var confUp; //confirm uppercase
- var confLo; // confirm lowercase
- var confNum; // confirm numbers
- var userSel; // user selection
+ var pwStringConcat = "";
  
- var specialCharacters = " ! # $ % &' * + -_ . / : ; < =  > ? @ ; "
+ var specialCharacters = "!#$&'*+-_./:;<=>?@;"
+ var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+ //var lower = "abcdefghijklmnopqrstuvwxyz"
+ var lower = upper.toLowerCase();
+ var numbers = "1234567890";
+ 
+ var pwLength = Number(window.prompt("How long do you want your password? Pick a number between 8 and 128."));
 
- var upper = ["A B C D E F G H I J K L M N O P Q R S T U V W X Y Z"]
- var lower = ["a b c d e f g h i j k l m n o p q r s t u v w x y z"]
- var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
- 
- 
- // Write password to the #password input
- function writePassword() {
-   var password = generatePassword();
-   var passwordText = document.querySelector("#password");
- 
-   passwordText.value = password;
 
+ // Logic for password length within parameters
+ if (pwLength >= 8 && pwLength <= 128) {
+   var finalPwLength = pwLength;
+ } else if (pwLength < 8 || pwLength > 128) {
+   window.prompt("Please enter a number between 8 & 128.");
  }
  
+ var pwUpper = window.confirm("Do you want your password to include upper-case letters?"); 
+ var pwLower = window.confirm("Do you want your password to include lower-case letters?"); 
+ var pwNumber = window.confirm("Do you want your password to include numbers?"); 
+ var pwSpecialCharacters = window.confirm("Do you want your password to include special characters?"); 
+
+ 
+
+if (pwSpecialCharacters != false) {
+    pwStringConcat += specialCharacters
+}
+
+if (pwUpper != false) {
+   pwStringConcat += upper
+}
+
+if (pwLower != false) {
+    pwStringConcat += lower
+}
+
+if (pwNumber != false) {
+    pwStringConcat += numbers
+}
+
+function generatePassword(length, characters) {
+  
+  //Transform string into an array
+  var charArray = characters.split("");
+  //length and amount of random characters from transformed array
+  var result = "";
+  //add length amount of random characters from charArray to result string
+  
+  //return result
+  return result;
+}
+
+ // Write password to the #password input
+ function writePassword() {
+  var password = generatePassword(pwLength, pwStringConcat);
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+
+
  // Add event listener to generate button
  generateBtn.addEventListener("click", writePassword);
 
- function generatePassword() {
-  pwLength = prompt("How long do you want your password? Pick a number between 8 and 128.");
-{
-  if(!pwLength) {
-    alert("Does not compute")
-  
-}
 
- else if (pwLength <= 8 && pwLength >= 128) {
-  passwordLength = prompt("You must choose between 8 and 128");
-  console.log("Password length " + pwLength);
-
-} else { 
-  confLo = confirm("Do you want lower-case characters?");
-  console.log("Lower case " + confLo);
-  confUp = confirm("Do you want upper-case characters?");
-  console.log("Upper case " + confUp);
-  confNum = confirm("Do you want numbers?");
-  console.log("Number " + confNum);
-  confSpec = confirm("Do you want special characters?");
-  console.log("Special Character " + confSpec);
-
-};
-
-// If no selection is made
-
-if (!confLo && !confUp && !confNum && !confSpec) {
-  userSel = window.prompt("You must choose a criteria");
-
-
-// all four choices are selected
-
-} else if (confLo && confUp && confNum && confSpec) {
-  userSel = lower.concat(upper, numbers, specialCharacters);
-  console.log(userSel);
-}
-// 3/4 choices are selected
-
-else if (confLo && confUp && confNum) {
-  userSel = lower.concat(upper, numbers);
-  console.log(userSel);
-}
-else if (confLo && confUp && confSpec) {
-  userSel = lower.concat(upper, specialCharacters);
-  console.log(userSel);
-}
-else if (confLo && confNum && confSpec) {
-  userSel = lower.concat(numbers, specialCharacters);
-  console.log(userSel);
-}
-else if (confUp && confNum && confSpec) {
-  userSel = upper.concat(numbers, specialCharacters);
-  console.log(userSel);
-}
-// half of choices are selected
-
-else if (confLo && confUp) {
-  userSel = lower.concat(upper);
-  console.log(userSel);
-}
-else if (confLo && confNum) {
-  userSel = lower.concat(numbers);
-  console.log(userSel);
-}
-else if (confLo && confSpec) {
-  userSel = lower.concat(specialCharacters);
-  console.log(userSel);
-}
-else if (confUp && confNum) {
-  userSel = upper.concat(numbers);
-  console.log(userSel);
-}
-else if (confUp && confSpec) {
-  userSel = upper.concat(specialCharacters);
-  console.log(userSel);
-}
-else if (confNum && confSpec) {
-  userSel = numbers.concat(specialCharacters);
-  console.log(userSel);
-}
-// 1/4 choices selected
-
-else if (confLo) {
-  userSel = lower;
-  console.log(userSel);
-}
-else if (confUp) {
-  userSel = blankUpper.concat(upper);
-  console.log(userSel);
-}
-else if (confNum) {
-  userSel = numbers;
-  console.log(userSel);
-}
-else if (confSpec) {
-  userSel = specialCharacters;
-  console.log(userSel);
-}
-}
-  // Empty variable for the password length
-  var passwordBlank = [];
-  
-  // Loop for random selection
-  for (var i = 0; i < pwLength; i++) {
-    var allChoices = userSel[Math.floor(Math.random() * userSel.length)];
-    passwordBlank.push(allChoices);
-    console.log(allChoices);
-  }
-
-  // Join and return the password 
-  var password = passwordBlank.join("");
-  console.log("Your Pasword is: " + password);
-  return writePassword();
-}
